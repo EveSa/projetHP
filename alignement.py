3fbf5d2ea746d3fd01488baf38571410f7ae45cd
Eve <sauvageve@gmail.com>
2022-03-02 13:24:53 +0100

    Clarification du fichier de sortie

--- alignement.py
+++ alignement.py
@@ -39,6 +39,10 @@ vrai_positif=0
 
 #### CORPS DU SCRIPT ####
 
+# Initialisation des titres des colonnes
+textes.write("Auto tokenisation         Manual tokenisation\n")
+
+#DEBUT DE LA BOUCLE
 while j < len(man_lines) and i <len(auto_lines): #On parcourt la totalité des lignes du fichiers
     
 #initialisation des valeurs pour la boucle en cours
@@ -51,8 +55,8 @@ while j < len(man_lines) and i <len(auto_lines): #On parcourt la totalité des l
 #on considère que c'est parceque le mot est réparti en plusieurs parties sur l'annotation manuelle
         if len(auto_word) > len(man_word) and re.search(man_word,auto_word) is not None and man_word[-1] != auto_word [-1] :
             
-            textes.write(auto_word+" " +man_word+"\n")
-            textes.write(auto_pos+" " +man_pos+"\n")
+            textes.write(auto_word+"\t\t\t\t"+man_word+"\n")
+            textes.write(auto_pos+"\t\t\t\t"+man_pos+"\n\n")
             print("-", end="")
             #On insère une nouvelle "ligne" dans le readlines pour décaler les sorties
             new_line = "(%s/%s)"%(auto_word,auto_pos)
@@ -61,8 +65,8 @@ while j < len(man_lines) and i <len(auto_lines): #On parcourt la totalité des l
 #et inversement
         elif len(auto_word) < len(man_word) and re.search(auto_word,man_word) is not None and man_word[-1] != auto_word [-1] :
 
-            textes.write(auto_word+" " +man_word+"\n")
-            textes.write(auto_pos+" " +man_pos+"\n")
+            textes.write(auto_word+"\t\t\t\t"+man_word+"\n")
+            textes.write(auto_pos+"\t\t\t\t"+man_pos+"\n\n")
             print("-", end="")
             
             new_line = "(%s/%s)"%(man_word,man_pos)
@@ -70,15 +74,13 @@ while j < len(man_lines) and i <len(auto_lines): #On parcourt la totalité des l
 
 #Si les deux mots ont la même taille, on peut regarder le POS et vérifier leur adéquation
         else:
-            textes.write(auto_word+" " +man_word+"\n")
-            textes.write(auto_pos+" " +man_pos+"\n")
             if auto_pos == man_pos :
                 vrai_positif +=1 
                 print("+", end="")
             
             else :
-                textes.write(auto_word+" " +man_word+"\n")
-                textes.write(auto_pos+" " +man_pos+"\n")
+                textes.write(auto_word+"\t\t\t\t"+man_word+"\n")
+                textes.write(auto_pos+"\t\t\t\t"+man_pos+"\n\n")
                 print("-", end="")
 
         i += 1
